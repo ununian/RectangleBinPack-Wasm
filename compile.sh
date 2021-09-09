b049c2ac3b61b3d96ee8c267bab914630579ff4d
ununian <ununian@outlook.com>
2021-09-09 22:49:04 +0800

    update

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 emcc --bind -Oz Warp.cc -o dist/Warp.js \
--s EXPORTED_RUNTIME_METHODS=['ccall','cwrap','addOnPostRun'] \
--s WASM=1 -s NO_EXIT_RUNTIME=1 \
+-s WASM=1 \
 -s MODULARIZE=1 \
 # -s ENVIRONMENT='node,web,worker' \
 # -s SINGLE_FILE=1 \
